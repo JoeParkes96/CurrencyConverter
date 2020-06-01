f51a2b5085c53b98716bad75b992099cc76664b5
JoeParkes96 <joeparkes1996@yahoo.co.uk>
2020-06-01 14:39:14 +0100

    Initial commit

---
+++ CurrencyConverter/Controllers/ConversionAuditController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConverter.Controllers
+{
+    public class ConversionAuditController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult ConversionAudit()
+        {
+            return View();
+        }
+    }
+}
