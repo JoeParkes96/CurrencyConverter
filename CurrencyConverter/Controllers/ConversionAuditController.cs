9e576dfe1b8c7b0e5129a851f1e785744d15bda4
JoeParkes96 <joeparkes1996@yahoo.co.uk>
2020-06-04 16:11:33 +0100

    Adding dummy SeedData and displaying in conversion audit table

--- CurrencyConverter/Controllers/ConversionAuditController.cs
+++ CurrencyConverter/Controllers/ConversionAuditController.cs
@@ -1,13 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CurrencyConverter.Models.ConversionAudit;
 
 namespace CurrencyConverter.Controllers
 {
     public class ConversionAuditController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult ConversionAudit()
+        private readonly ConversionAuditContext _context;
+
+        public ConversionAuditController(ConversionAuditContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ConversionAudit
+        public async Task<IActionResult> ConversionAudit()
+        {
+            return View(await _context.Conversion.ToListAsync());
+        }
+
+        // GET: ConversionAudit/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conversion = await _context.Conversion
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (conversion == null)
+            {
+                return NotFound();
+            }
+
+            return View(conversion);
+        }
+
+        // GET: ConversionAudit/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: ConversionAudit/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FromCurrencyName,ToCurrencyName,AmountConverted,ConversionResult,DateSubmitted")] Conversion conversion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(conversion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(ConversionAudit));
+            }
+            return View(conversion);
         }
     }
 }
---
+++ CurrencyConverter/Models/ConversionAudit/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace CurrencyConverter.Models.ConversionAudit
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ConversionAuditContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ConversionAuditContext>>()))
+            {
+                // Look for any conversions.
+                if (context.Conversion.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                // Adding dummy entries to enable useful date querying
+                context.Conversion.AddRange(
+                    new Conversion
+                    {
+                        FromCurrencyName = "GBP",
+                        ToCurrencyName = "EUR",
+                        AmountConverted = 12345m,
+                        ConversionResult = 13816.28m,
+                        DateSubmitted = new DateTime(2019, 07, 24)
+                    },
+                    new Conversion
+                    {
+                        FromCurrencyName = "AUD",
+                        ToCurrencyName = "USD",
+                        AmountConverted = 250.75m,
+                        ConversionResult = 173.75m,
+                        DateSubmitted = new DateTime(2020, 01, 12)
+                    },
+                    new Conversion
+                    {
+                        FromCurrencyName = "EUR",
+                        ToCurrencyName = "GBP",
+                        AmountConverted = 95.23m,
+                        ConversionResult = 85.09m,
+                        DateSubmitted = new DateTime(2020, 06, 04)
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
