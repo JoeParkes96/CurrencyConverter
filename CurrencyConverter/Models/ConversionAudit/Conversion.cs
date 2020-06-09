2205acc3d7dab9ee187a363614a5a447669dd8d1
JoeParkes96 <joeparkes1996@yahoo.co.uk>
2020-06-09 15:37:23 +0100

    Adding conversion audit context

---
+++ CurrencyConverter/Models/ConversionAudit/Conversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CurrencyConverter.Models.ConversionAudit
+{
+    public class Conversion
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "From Currency Name")]
+        public string FromCurrencyName { get; set; }
+
+        [Display(Name = "To Currency Name")]
+        public string ToCurrencyName { get; set; }
+
+        [Display(Name = "Amount Converted")]
+        public decimal AmountConverted { get; set; }
+
+        [Display(Name = "Conversion Result")]
+        public decimal ConversionResult { get; set; }
+
+        [Display(Name = "Date Submitted")]
+        [DataType(DataType.Date)]
+        public DateTime DateSubmitted { get; set; }
+    }
+}
---
+++ CurrencyConverter/Models/ConversionAudit/ConversionAuditContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyConverter.Models.ConversionAudit
+{
+    public class ConversionAuditContext : DbContext
+    {
+        public ConversionAuditContext(DbContextOptions<ConversionAuditContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<Conversion> Conversion { get; set; }
+    }
+}
